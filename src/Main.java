448bbf8a9ab8361b36c15676a1401d6d0bc7f0fd
Valeria Álvarez <valalvarez@cesde.net>
2026-07-28 20:30:06 -0500

    Se genera menu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Menu");
+        System.out.println("Entradas:");
+        System.out.println("Sushi:");
+        System.out.println("Ramen:");
     }
 }
